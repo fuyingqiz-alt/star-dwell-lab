30650dc28b2965372f3512d10aa571db0b0154ae
fuyingqiz-alt <fuyingqiz@gmail.com>
2026-04-08 22:57:12 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({base: '/star-dwell-lab/'
+export default defineConfig({
+  base: '/star-dwell-lab/'
   plugins: [react()],
   test: {
     environment: "jsdom",
